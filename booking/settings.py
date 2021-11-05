1540ec7e28ca5417ae95bd6b93af2ae6d1cfab8f
Rads059 <69387648+Rads059@users.noreply.github.com>
2021-11-06 00:13:03 +0530

    Update settings.py

--- booking/settings.py
+++ booking/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!-sfi#(o-t94c83#)ct6o*&+gb+afkqm()^v%t0ndobml73)#%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -124,7 +124,75 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+SIMPLE_JWT = {
+
+'ACCESS_TOKEN_LIFETIME':timedelta(minutes=60),
+
+'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+
+'ROTATE_REFRESH_TOKENS': False,
+
+'BLACKLIST_AFTER_ROTATION': True,
+
+'UPDATE_LAST_LOGIN': False,
+
+
+
+'ALGORITHM': 'HS256',
+
+'SIGNING_KEY':' settings.SECRET_KEY',
+
+'VERIFYING_KEY': None,
+
+'AUDIENCE': None,
+
+'ISSUER': None,
+
+
+
+'AUTH_HEADER_TYPES': ('Bearer',),
+
+'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+
+'USER_ID_FIELD': 'id',
+
+'USER_ID_CLAIM': 'user_id',
+
+'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+
+
+'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+
+'TOKEN_TYPE_CLAIM': 'token_type',
+
+
+
+'JTI_CLAIM': 'jti',
+
+
+
+'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+
+'SLIDING_TOKEN_LIFETIME':timedelta(minutes=5),
+
+'SLIDING_TOKEN_REFRESH_LIFETIME':timedelta(days=1),
+
+}
